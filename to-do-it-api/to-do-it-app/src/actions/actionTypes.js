21094d85cf00bf1609dafd268f32623ebfdfd49d
Anthony Oglesby <ogle1ant@gmail.com>
2020-04-15 15:25:27 -0400

    fixed minor issues during testing

--- to-do-it-api/to-do-it-app/src/actions/actionTypes.js
+++ to-do-it-api/to-do-it-app/src/actions/actionTypes.js
@@ -1,4 +1,4 @@
 export const LOAD_TODOS = 'LOAD_TODOS'
-export const ADD_TODOS = 'ADD_TODO'
-export const TOGGLE_TODOS = 'TOGGLE_TODO'
-export const DELETE_TODOS = 'DELETE_TODO'
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const DELETE_TODO = 'DELETE_TODO'
